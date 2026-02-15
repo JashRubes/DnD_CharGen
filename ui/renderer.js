b803dec644b7dcfaefbeec4222dcf22ccbe3ed40
JashRubes <125108352+JashRubes@users.noreply.github.com>
2026-02-14 19:45:14 -0500

    Update renderer.js

--- ui/renderer.js
+++ ui/renderer.js
@@ -1,1 +1,11 @@
+export function renderOptions(containerId, dataList, onClick) {
+    const container = document.getElementById(containerId);
+    container.innerHTML = "";
 
+    dataList.forEach(item => {
+        const btn = document.createElement("button");
+        btn.textContent = item.name;
+        btn.onclick = () => onClick(item);
+        container.appendChild(btn);
+    });
+}
