90c7a1381c29cbbe5d009df3f06f681abdb88351
JashRubes <125108352+JashRubes@users.noreply.github.com>
2026-02-14 19:44:44 -0500

    Update character.js

--- logic/character.js
+++ logic/character.js
@@ -1,1 +1,20 @@
+export class Character {
+    constructor() {
+        this.class = null;
+        this.race = null;
+        this.background = null;
+        this.level = 1;
+    }
 
+    setClass(classData) {
+        this.class = classData;
+    }
+
+    setRace(raceData) {
+        this.race = raceData;
+    }
+
+    getHitDie() {
+        return this.class ? this.class.hitDie : null;
+    }
+}
